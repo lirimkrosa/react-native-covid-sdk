a1166d1a26fa689514dc682e6395869744732c8b
Lee <lee@pop-os.localdomain>
2021-09-29 20:04:03 +0200

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# react-native-covid-sdk
+
+React Native Certificate Verifier for Europe
+
+## Installation
+
+```sh
+npm install react-native-covid-sdk
+```
+
+## Usage
+
+```js
+import { multiply } from "react-native-covid-sdk";
+
+// ...
+
+const result = await multiply(3, 7);
+```
+
+## Contributing
+
+See the [contributing guide](CONTRIBUTING.md) to learn how to contribute to the repository and the development workflow.
+
+## License
+
+MIT
---
+++ android/src/main/java/com/reactnativecovidsdk/CovidSdkModule.kt
@@ -0,0 +1,24 @@
+package com.reactnativecovidsdk
+
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactContextBaseJavaModule
+import com.facebook.react.bridge.ReactMethod
+import com.facebook.react.bridge.Promise
+
+class CovidSdkModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
+
+    override fun getName(): String {
+        return "CovidSdk"
+    }
+
+    // Example method
+    // See https://reactnative.dev/docs/native-modules-android
+    @ReactMethod
+    fun multiply(a: Int, b: Int, promise: Promise) {
+    
+      promise.resolve(a * b)
+    
+    }
+
+    
+}
